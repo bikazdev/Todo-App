a8f6d41926c06e5e770db59b65a2d9aedfd6ce31
bikazDev <bikazdev@gmail.com>
2026-07-09 16:38:32 +0330

    fix bg photo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import TodoMain from "./components/Todo/TodoMain";
 const App = () => {
   return (
     <div
-      className="w-full z-50 dark:bg-gray-900 bg-zinc-50 dark:bg-[url('./images/bg-mobile-dark.jpg')] bg-[url('./images/bg-mobile-light.jpg')] bg-size-[100%_250px] lg:bg-size-[100%_330px] bg-no-repeat min-h-screen overflow-y-auto pb-10 lg:bg-[url('./images/bg-desktop-light.jpg')] lg:dark:bg-[url('./images/bg-desktop-dark.jpg')] transition-colors 
+      className="w-full z-50 dark:bg-gray-900 bg-zinc-50 dark:bg-[url('/images/bg-mobile-dark.jpg')] bg-[url('/images/bg-mobile-light.jpg')] bg-size-[100%_250px] lg:bg-size-[100%_330px] bg-no-repeat min-h-screen overflow-y-auto pb-10 lg:bg-[url('/images/bg-desktop-light.jpg')] lg:dark:bg-[url('/images/bg-desktop-dark.jpg')] transition-colors 
   duration-500 
   ease-in-out"
     >
